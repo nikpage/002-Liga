6d383345a14941d6d105bbec5ccca331c2aea186
Nik Page <nbpage@gmail.com>
2025-12-22 21:56:09 +0100

    refactored

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  neo4j: {
+    uri: process.env.NEO4J_URI,
+    user: process.env.NEO4J_USER,
+    pass: process.env.NEO4J_PASS
+  },
+  google: {
+    key: process.env.GOOGLE_API_KEY,
+    embModel: "models/embedding-001"
+  }
+};
