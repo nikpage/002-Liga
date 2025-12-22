d78b19f8aacf3700b1cdc07acb39a3b84e658dbc
Nik Page <nbpage@gmail.com>
2025-12-22 23:50:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express=require("express");
+const fetch=require("node-fetch");
 const app=express();
 app.use(express.static("."));
+app.post("/search", async (req,res)=>{
+  const r=await fetch("https://europe-west1-gen-lang-client-0651826784.cloudfunctions.net/search",{method:"POST"});
+  res.send(await r.text());
+});
 app.listen(process.env.PORT||8080);
