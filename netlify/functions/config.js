9f419fc499766e97d03644bbabf57129db6e4cae
Nik Page <nbpage@gmail.com>
2025-12-29 12:06:57 +0100

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -2,11 +2,11 @@ module.exports = {
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_SERVICE_ROLE_KEY,
-    tableName: "document_chunks" 
+    tableName: "document_chunks"
   },
   google: {
     key: process.env.GOOGLE_API_KEY,
     embModel: "gemini-embedding-001",
-    chatModel: "gemini-2.0-flash-lite"
+    chatModel: "gemini-2.5-flash"
   }
 };
